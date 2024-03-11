8abaac2888bb4fcb192c55d15be071b939f0e6ae
Jesica Sousa <89730976+JesicaSousa@users.noreply.github.com>
2024-03-11 18:39:01 -0300

    post

--- src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
@@ -4,15 +4,14 @@ import java.io.Serializable;
 
 import com.nelioalves.workshopmongo.domain.User;
 
-public class UserDTO implements Serializable{
+public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private String id;
 	private String name;
 	private String email;
 	
 	public UserDTO() {
-		
 	}
 	
 	public UserDTO(User obj) {
@@ -44,5 +43,4 @@ public class UserDTO implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
 }
